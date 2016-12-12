9a42bb9402c8480a8836b3724f21d9083305d239
Klarizon Emar Motol <emar@localhost.localdomain>
2016-12-12 20:43:11 +0800

    Update repo name

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'emarojt_portfolio');
+define('DB_NAME', 'emar_ojt_portfolio');
 
 /** MySQL database username */
 define('DB_USER', 'root');
